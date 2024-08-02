b351aa8617a17d5626906014280e58f8710ea9de
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-02 17:24:10 -0400

    added gemini fucntionality and recipie generation

--- app/actions.ts
+++ app/actions.ts
@@ -3,9 +3,15 @@
 import prisma from "@/lib/db";
 import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
+import { google } from "@ai-sdk/google";
+import { streamText } from "ai";
+import { createStreamableValue } from "ai/rsc";
 
 const id = 'cfghyjcrgrd';
-
+export interface Message {
+    role: "user" | "assistant";
+    content: string;
+  }
 
 type FormState = {
     message: string;
@@ -14,6 +20,66 @@ type FormState = {
 const generateRandomId = () => {
     return Math.random().toString(36).substring(2, 10);
 };
+
+const conversationHistory: Record<string, Message[]> = {};
+
+export async function streamRecipes(history: Message[]) {
+    const stream = createStreamableValue();
+    const model = google("models/gemini-1.5-pro-latest");
+  
+    const sanitizeText = (text: string,) => text.replace(/[*_~`]/g, '');
+  
+    const prompt = `You are a smart chef in a "pantry app" that people use to keep track of their food. You have a great personality. Your responses should be
+    only a recipe based on your user is asking. This means do Not INCLUDE THE PROMPT IN YOUR RESPONSE.
+
+    - your responses need ot be in this format, NO EXCEPTIONS:
+    Search your pantry
+
+    ## (TItle of Recipe)
+
+    Servings: 'Insert serving size here'
+    Prep Time: 'Insert preparation time here'
+
+    Ingredients:
+    1 ingredient
+    2 ingredient
+    3 ingredient
+    etc...
+
+    Instructions:
+    instructions 1
+    instructions 2
+    instructions 3
+    etc...
+
+        
+    Here's the conversation so far:
+    
+    ${history.map(msg => `${msg.role === "user" ? "User" : "Assistant"}: ${sanitizeText(msg.content)}`).join('\n')}
+    
+    Your response should combine all these elements in a balanced way.`;
+    
+    (async () => {
+      const { textStream } = await streamText({
+        model: model,
+        messages: [{ role: "user", content: prompt }],
+        temperature: 0.9, 
+        topP: 0.85,
+        topK: 40,
+      });
+  
+      for await (const text of textStream) {
+        stream.update(sanitizeText(text));
+      }
+  
+      stream.done();
+    })().then(() => {});
+  
+    return {
+      messages: history,
+      newMessage: stream.value,
+    };
+  }
 
 
 export async function submitPantryItems(formData: FormData, formState: FormState) {
--- app/home-components/C_hero.tsx
+++ app/home-components/C_hero.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 import ClientAdd from "./C_add";
 import { usePantry } from "../contextProviders/pantryContext";
 import { AnimatePresence, motion } from "framer-motion";
-import { getPantryItems, deletePantryItems } from "../actions";
+import { getPantryItems, deletePantryItems, Message, streamRecipes } from "../actions";
 import { useEffect, useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { useFormState, useFormStatus } from "react-dom";
@@ -14,6 +14,7 @@ import {
   } from "@/components/ui/popover"
 import { FaSearch } from "react-icons/fa";
 import Image  from "next/image";
+import { readStreamableValue } from "ai/rsc";
 
 // local states
 interface PantryItem {
@@ -43,6 +44,16 @@ export default function ClientHero( { items }: { items: PantryItem[] } ) {
         item.ItemTitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
         (item.ItemQuantity !== undefined && item.ItemQuantity.toString().includes(searchQuery))
     );
+    const [selectedItems, setSelectedItems] = useState<string[]>([]);
+    const [conversation, setConversation] = useState<Message[]>([]);
+    const [recipe, setRecipe] = useState<string>('');
+    const handleItemClick = (itemId: string) => {
+        if (selectedItems.includes(itemId)) {
+            setSelectedItems(selectedItems.filter(item => item !== itemId));
+        } else {
+            setSelectedItems([...selectedItems, itemId]);
+        }
+    };
 
     // Local functions
     const handleDelete = async (prevState: FormState, formData: FormData): Promise<FormState> => {
@@ -82,6 +93,75 @@ export default function ClientHero( { items }: { items: PantryItem[] } ) {
         message: '',
     });
 
+    const startChat = async () => {
+        const input = "I need you to come up with one recipe based on the items in my pantry. here are the items: " + selectedItems.join(', ');
+        // console.log(input)
+        const { messages, newMessage } = await streamRecipes([
+            ...conversation,
+            { role: "user", content: input },
+        ]);
+
+        let textContent = "";
+
+        for await (const delta of readStreamableValue(newMessage)) {
+            textContent = `${textContent}${delta}`;
+
+            // setConversation([
+            //     ...messages,
+            //     { role: "assistant", content: textContent },
+            // ]);
+
+            setRecipe(textContent);
+        }
+
+
+    }
+
+    function formatRecipe(messageContent: string) {
+        // Split message content by lines
+        const lines = messageContent.split('\n').filter(line => line.trim() !== '');
+    
+        // Extract different parts of the recipe
+        let recipeContent = `
+            <div>
+        `;
+    
+        const titleLine = lines.find(line => line.startsWith('##'));
+        if (titleLine) {
+            recipeContent += `<h1>${titleLine}</h1><br>`;
+        }
+    
+        const servingsLine = lines.find(line => line.startsWith('Servings:'));
+        if (servingsLine) {
+            recipeContent += `<p><strong>${servingsLine}</strong></p><br>`;
+        }
+    
+        const prepTimeLine = lines.find(line => line.startsWith('Prep Time:'));
+        if (prepTimeLine) {
+            recipeContent += `<p><strong>${prepTimeLine}</strong></p><br>`;
+        }
+    
+        const ingredientsIndex = lines.findIndex(line => line.startsWith('Ingredients:'));
+        const instructionsIndex = lines.findIndex(line => line.startsWith('Instructions:'));
+    
+        if (ingredientsIndex !== -1 && instructionsIndex !== -1) {
+            const ingredientsLines = lines.slice(ingredientsIndex + 1, instructionsIndex).join('<br>');
+            recipeContent += `<h3>Ingredients:</h3><p>${ingredientsLines}</p><br>`;
+    
+            const instructionsLines = lines.slice(instructionsIndex + 1)
+                .map((line, index) => `${line}</p><br>`)
+                .join('');
+            recipeContent += `<h3>Instructions:</h3><div>${instructionsLines}</div>`;
+        }
+    
+        recipeContent += `
+            </div>
+        `;
+    
+        return recipeContent;
+    }
+    
+
     // useEffects
     useEffect(() => {
         const fetchItems = async () => {
@@ -106,14 +186,42 @@ export default function ClientHero( { items }: { items: PantryItem[] } ) {
                 
                 <div className="w-full overflow-hidden min-h-full flex flex-col  gap-4 p-5">
                 <h1 className="place-self-start flex text-3xl font-bold select-none">Hello {name}.</h1>
+                <div className="flex justify-between w-full h-max">     
                 <Input
-                        id='lcs3'
+                id='lcs3'
                         type="text"
                         placeholder="Search your pantry"
                         value={searchQuery}
                         onChange={handleSearchChange}
                         className="w-1/2 place-self-start outline-transparent flex place-items-center outline p-2 rounded-xl px-4"
-                    />
+                />
+                {selectedItems.length > 0 && (                    
+                    <AnimatePresence>
+                        <motion.div 
+                            onClick={startChat}
+                            initial={{ 
+                                opacity: 0,
+                                y: 100
+                            }}
+                            animate={{ 
+                                opacity: 1,
+                                y: 0 
+                            }}
+                            exit={{ 
+                                opacity: 0,
+                                y: 10
+                            }}
+                            whileHover={{ 
+                                scale: 1.05 
+                            }}
+                                        
+                        className="cursor-pointer mr-5 w-[300px] h-max p-2 bg-white rounded-xl text-black flex place-content-center place-items-center">
+                            <p className="text-center select-none">Generate recipies</p>
+                        </motion.div>
+                    </AnimatePresence>
+                )}
+
+                </div>
                 <div className="pt-[5vh]  w-full h-[80vh] sm:h-[50vh] overflow-y-scroll p-5 place-items-center grid grid-cols-1 sm:grid-cols-4 gap-[6px]">
                     {filteredPosts.map((item, index) => (
                         <>
@@ -135,8 +243,8 @@ export default function ClientHero( { items }: { items: PantryItem[] } ) {
                                 scale: 1.05 
                             }}
                             
-                            key={item.id} className="w-[100%] h-[200px] outline outline-[1px] relative place-items-end rounded-xl flex gap-4 justify-between p-3" style={{ backgroundImage: `url(${item.ItemImagePexals as string})`, backgroundRepeat: 'no-repeat', backgroundSize: 'fill' }}>                
-                                <span className="cursor-pointer select-none bg-neutral-600 text-white rounded-xl px-3 py-1 font-bold" onClick={() => toggleAdd(item.id, item.ItemTitle, item.ItemQuantity)}>{item.ItemTitle}</span>
+                            key={item.id} onClick={() => handleItemClick(item.ItemTitle)} className={`w-[100%] h-[200px] outline outline-[1px] relative place-items-end rounded-xl flex gap-4 justify-between p-3 ${selectedItems.includes(item.ItemTitle) ? 'outline-green-500 outline-[4px]' : ''} ${currentItem === item.ItemTitle ? 'outline-blue-500' : ''}`} style={{ backgroundImage: `url(${item.ItemImagePexals as string})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}>                
+                                <span className="cursor-pointer select-none bg-neutral-600 text-white rounded-xl px-3 py-1 font-bold"  onClick={() => toggleAdd(item.id, item.ItemTitle, item.ItemQuantity)}>{item.ItemTitle}</span>
                                 <motion.span onClick={() => toggleAdd(item.id, item.ItemTitle, item.ItemQuantity)} className="absolute select-none cursor-pointer top-[-3%] left-[-3%] px-3 rounded-full bg-white text-neutral-700 font-bold"
                                     whileHover={{ scale: 1.2 }}
                                     >
@@ -159,27 +267,32 @@ export default function ClientHero( { items }: { items: PantryItem[] } ) {
                         </AnimatePresence>
                         </>
                     ))}
-                    <AnimatePresence>
-                        <motion.div 
-                        initial={{ 
-                            opacity: 0,
-                            y: 100
-                        }}
-                        animate={{ 
-                            opacity: 1,
-                            y: 0 
-                        }}
-                        exit={{ 
-                            opacity: 0,
-                            y: 10
-                        }}
-                        whileHover={{ 
-                            scale: 1.05 
-                        }}
-                        className="cursor-pointer w-[100%] h-[200px] bg-white rounded-xl text-black flex place-content-center place-items-center">
-                            <p>Generate recipies</p>
-                        </motion.div>
-                    </AnimatePresence>
+
+                    {/* generate recipies */}
+                    {selectedItems.length > 0 && (
+                        <AnimatePresence>
+                            <motion.div
+                            onClick={startChat} 
+                            initial={{ 
+                                opacity: 0,
+                                y: 100
+                            }}
+                            animate={{ 
+                                opacity: 1,
+                                y: 0 
+                            }}
+                            exit={{ 
+                                opacity: 0,
+                                y: 10
+                            }}
+                            whileHover={{ 
+                                scale: 1.05 
+                            }}
+                            className="cursor-pointer w-[100%] h-[200px] bg-white rounded-xl text-black flex place-content-center place-items-center">
+                                <p className="text-center select-none">Generate recipies</p>
+                            </motion.div>
+                        </AnimatePresence>
+                    )}
                     <ClientAdd items={items} />
                 </div>
             </div>
@@ -192,6 +305,55 @@ export default function ClientHero( { items }: { items: PantryItem[] } ) {
                     <ClientAdd items={items} />
                 </>
             )}
+
+            {/* {conversation && ( */}
+            {recipe.length > 0 && (
+                <motion.div className="flex place-content-center w-full absolute bottom-[20%] left-0"
+                    initial={{ 
+                        opacity: 0,
+                        y: 100
+                    }}
+                    animate={{ 
+                        opacity: 1,
+                        y: 0 
+                    }}
+                    exit={{ 
+                        opacity: 0,
+                        y: 10
+                    }}
+                >
+                    <div className="w-full h-full flex flex-col gap-4 place-items-center">
+                        <div className="relative w-3/4 bg-neutral-600 sm:w-1/2 h-[400px] overflow-y-scroll rounded-xl p-4 py-8">
+                            
+                            <motion.div 
+                            whileHover={{ scale: 1.05 }}
+                            onClick={() => setRecipe('')} className="cursor-pointer w-max p-3 flex place-items-center h-[20px] bg-red-500 absolute top-3 right-3 rounded-xl">
+                                <span>Close</span>
+                            </motion.div>
+
+                            <div className="w-full h-[400px] overflow-y-scroll">
+                                {/* {conversation.map((message, index) => (
+                                    <div className='w-[90%] m-2 rounded-md p-2' key={index}>
+                                        <span>
+                                            <div dangerouslySetInnerHTML={{ __html: formatRecipe(message.content) }} />
+                                        </span>
+                                    </div>
+                                ))} */}
+                                  <div className='w-[90%] m-2 rounded-md p-2'>
+                                        <span>
+                                            <div dangerouslySetInnerHTML={{ __html: formatRecipe(recipe) }} />
+                                        </span>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+            )}
+
+
+            
+
+
         </>
     )
 }
